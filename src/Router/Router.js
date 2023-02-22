a88a24454cbfa1f14b694af05ded53920a789aff
naymurweb <naymurweb@gmail.com>
2023-02-22 22:35:36 +0600

    file setup

---
+++ src/Router/Router.js
@@ -0,0 +1,9 @@
+const { createBrowserRouter } = require("react-router-dom");
+const { default: Main } = require("../Layout/Main");
+
+const router=createBrowserRouter([
+    {path:'/',element:<Main></Main>,children:[
+
+    ]}
+])
+export default router
