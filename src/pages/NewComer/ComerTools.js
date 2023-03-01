0379b6245adbcbf1860bab4d4ee90c7595d71c3e
naymur~ <naymurweb@gmail.com>
2023-03-02 01:32:42 +0600

    pagination

--- src/pages/NewComer/ComerTools.js
+++ src/pages/NewComer/ComerTools.js
@@ -1,14 +1,20 @@
 import React, { useEffect, useState } from "react";
-import ButtonSecondary from "../../components/ButtonSecondary";
 import ToolCard2 from "../../components/ToolCard2";
+import "../../stylesheet/pagination.css";
 
 const ComerTools = () => {
   const [tool, setTool] = useState([]);
+  const [page, setPage] = useState(0);
+  // pagination
+  const count = tool.length;
+  const parPage = 6;
+  const pages = Math.ceil(count / parPage);
   useEffect(() => {
-    fetch("http://localhost:9000/newtools")
+    fetch(`http://localhost:9000/newtools`)
       .then((res) => res.json())
       .then((data) => setTool(data));
-  }, []);
+  }, [pages]);
+
   return (
     <section className="container mx-auto px-5 my-12">
       <div className="grid md:grid-cols-2 gap-8">
@@ -17,7 +23,15 @@ const ComerTools = () => {
         ))}
       </div>
       <div className="text-center my-12">
-        <ButtonSecondary color={"bg-primaryDark"}>See All</ButtonSecondary>
+        {[...Array(pages).keys()].map((number) => (
+          <button
+            className={`btn bg-green-400 mx-2 ${page === number && "selected"}`}
+            onClick={() => setPage(number)}
+            key={number}
+          >
+            {number + 1}
+          </button>
+        ))}
       </div>
     </section>
   );
